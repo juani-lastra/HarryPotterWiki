d45a230a58fd9454a49665543429fcbe35c6e611
juan.lastra <juan.lastra@ticketmaster.com>
2022-02-25 13:42:32 -0300

    splash activity
    pr comments
    
    change data status name
    
    general dependencies gradle file
    
    cr comment

--- app/src/main/java/com/mobiletandil/harrypotterwiki/activity/MainActivity.kt
+++ app/src/main/java/com/mobiletandil/harrypotterwiki/activity/MainActivity.kt
@@ -1,11 +1,18 @@
-package com.mobiletandil.harrypotterwiki
+package com.mobiletandil.harrypotterwiki.activity
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.mobiletandil.harrypotterwiki.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    companion object {
+        fun getIntent(context: Context) = Intent(context, MainActivity::class.java)
     }
 }
